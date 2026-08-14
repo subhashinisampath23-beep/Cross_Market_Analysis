1187d2a3d0b3212daec116d222080791dd473971
subhashinisampath23-beep <subhashinisampath23@gmail.com>
2026-08-14 14:47:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Cross-Market Analysis",
+    layout="wide"
+)
+
+st.title("Cross-Market Analysis")
+
+st.subheader("Crypto, Oil and Stock Market Analytics")
+
+st.write(
+    """
+    This application analyzes cryptocurrency, crude oil,
+    and stock-market data using SQL and Streamlit.
+    """
+)
